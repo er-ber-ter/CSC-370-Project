12e0d55f6338ef027034c8dbb4a8f2d03dd18ed0
erberter <albertgorenstein@uvic.ca>
2024-06-02 19:53:48 -0700

    added join script

--- Join.sql
+++ Join.sql
@@ -4,4 +4,4 @@ ADD COLUMN title VARCHAR(100);
 UPDATE champion
 JOIN champion_names ON champion.name = champion_names.name
 SET champion.title = champion_names.title
-WHERE champion_names.title IS NOT NULL;
+WHERE champion.title IS NULL;
